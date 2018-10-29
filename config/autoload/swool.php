7af6d5f15a509e58275f4d6a6dbb7953a03b6469
James Stormes <jstormes@stormes.net>
2018-10-29 14:48:12 -0500

    Swool config.

---
+++ config/autoload/swool.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jstormes
+ * Date: 10/29/2018
+ * Time: 2:47 PM
+ */
+
+return [
+    'zend-expressive-swoole' => [
+        'swoole-http-server' => [
+            'host' => '0.0.0.0',
+            'port' => 8080
+        ],
+    ],
+];
